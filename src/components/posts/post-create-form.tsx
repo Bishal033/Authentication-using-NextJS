1f71512af811c6a33a4bb6439bad576b61c8d3da
Bishal Das <vdas95070@email.com>
2026-08-17 21:47:49 +0530

    Validation in Create post

--- src/components/posts/post-create-form.tsx
+++ src/components/posts/post-create-form.tsx
@@ -1,3 +1,53 @@
+'use client';
+
+import { useActionState } from "react"
+import{
+    Input,
+    Button,
+    Textarea,
+    Popover,
+    PopoverTrigger,
+    PopoverContent
+} from '@nextui-org/react'
+import * as actions from '@/actions'
+import FromButton from "../common/form-button";
+
+
 export default function PostCreateForm(){
-    return <div>Post Create form</div>
+    const [formState, action] = useActionState(actions.createPost,{
+        errors:{}
+    })
+    return (
+        <Popover placement="left">
+            <PopoverTrigger>
+                <Button color="primary">
+                    Create a Post
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+                <form action={action}>
+                    <div className="flex flex-col gap-4 p-4 w-80">
+                    <h3 className="text-lg">Create a Post</h3>
+                    <Input
+                    isInvalid={!!formState.errors.title}
+                    errorMessage={formState.errors.title?.join(',')}
+                    name="title"
+                    label="Title"
+                    labelPlacement="outside"
+                    placeholder="Title"
+                    />
+                    <Textarea
+                    isInvalid={!!formState.errors.content}
+                    errorMessage={formState.errors.content?.join(',')}
+                    name="content"
+                    label="Content"
+                    labelPlacement="outside"
+                    placeholder="Content"
+                    />
+                    <FromButton>Create Post</FromButton>
+                    </div>
+                </form>
+            </PopoverContent>
+        </Popover>
+    )
 }
