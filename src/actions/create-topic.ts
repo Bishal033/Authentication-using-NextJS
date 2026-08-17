fd593daa700e7894d21613405af88665382294cf
Bishal Das <vdas95070@email.com>
2026-08-17 13:41:34 +0530

    Create topic redirect

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,13 @@
 'use server';
 
+import type {Topic} from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+import {redirect} from 'next/navigation';
 import {z} from 'zod';
+import {auth} from '@/auth';
+import { db } from '@/db';
+import paths from '@/path';
+import { error } from 'console';
 const createTopicSchema = z.object({
     name: z.string().min(3).regex(/[a-z-]/, {message: 'Must be lowercase letters or dashes without spaces'}),
     description: z.string().min(10),
@@ -10,6 +17,7 @@ interface CreateTopicFormState{
     errors:{
         name?: string[];
         description?: string[];
+        _form?: string[];
     }
 }
 
@@ -24,10 +32,40 @@ export async function createTopic(formState: CreateTopicFormState, formData:Form
             errors: result.error.flatten().fieldErrors
         }
     }
+    
+    const session = await auth();
+    if(!session || !session.user){
+        return{
+            errors:{
+                _form: ['You must be signed in to create a Topic.']
+            }
+        }
+    }
 
-    return {
-        errors: {}
-    };
-  
-    //TODO: revalidate the homepage
+    let topic: Topic;
+
+    try{
+        topic = await db.topic.create({
+        data:{
+            slug: result.data.name,
+            description: result.data.description
+        }
+        })
+    } catch(err:unknown) {
+        if(err instanceof Error){
+            return{
+                errors: {
+                    _form: [err.message]
+                }
+            }
+        } else{
+            return{
+                errors: {
+                    _form: ['Something went wrong']
+                }
+            }
+        }
+    }
+    revalidatePath('/')
+    redirect(paths.topicShow(topic.slug))
 }
